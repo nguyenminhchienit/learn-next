b1c7d378b53cac06e73a4a69eaaca71a6039dc2c
NguyenMinhChien <minhchien03112003@gmail.com>
2024-08-27 19:50:56 +0700

    login form

--- src/app/(auth)/login/login-form.tsx
+++ src/app/(auth)/login/login-form.tsx
@@ -1,8 +1,5 @@
 "use client";
-import {
-  RegisterBody,
-  RegisterBodyType,
-} from "@/schemaValidations/auth.schema";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
@@ -17,22 +14,21 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import envConfig from "@/configs/config";
+import { LoginBody, LoginBodyType } from "@/schemaValidations/auth.schema";
 
-const RegisterForm = () => {
-  const form = useForm<RegisterBodyType>({
-    resolver: zodResolver(RegisterBody),
+const LoginForm = () => {
+  const form = useForm<LoginBodyType>({
+    resolver: zodResolver(LoginBody),
     defaultValues: {
-      name: "",
       email: "",
       password: "",
-      confirmPassword: "",
     },
   });
 
   // 2. Define a submit handler.
-  async function onSubmit(values: RegisterBodyType) {
+  async function onSubmit(values: LoginBodyType) {
     const result = await fetch(
-      `${envConfig.NEXT_PUBLIC_API_ENDPOINT}/auth/register`,
+      `${envConfig.NEXT_PUBLIC_API_ENDPOINT}/auth/login`,
       {
         body: JSON.stringify(values),
         method: "POST",
@@ -42,7 +38,7 @@ const RegisterForm = () => {
       }
     ).then((res) => res.json());
 
-    console.log("result: ", result);
+    console.log("res: ", result);
   }
 
   return (
@@ -51,20 +47,6 @@ const RegisterForm = () => {
         onSubmit={form.handleSubmit(onSubmit)}
         className="space-y-2 max-w-[500px] w-full"
       >
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Tên</FormLabel>
-              <FormControl>
-                <Input placeholder="Tên người dùng" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
         <FormField
           control={form.control}
           name="email"
@@ -93,29 +75,12 @@ const RegisterForm = () => {
           )}
         />
 
-        <FormField
-          control={form.control}
-          name="confirmPassword"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Xác nhận mật khẩu</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="Xác nhận mật khẩu"
-                  type="password"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
         <Button type="submit" className="w-full !mt-8">
-          Đăng ký
+          Đăng nhập
         </Button>
       </form>
     </Form>
   );
 };
 
-export default RegisterForm;
+export default LoginForm;
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,16 @@
-
-
-import React from 'react'
+import React from "react";
+import LoginForm from "./login-form";
 
 const page = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 3000))
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return (
-    <div>
-      Login
+    <div className="my-auto w-full">
+      <h1 className="flex justify-center text-xl font-bold">Đăng nhập</h1>
+      <div className="flex justify-center">
+        <LoginForm />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
